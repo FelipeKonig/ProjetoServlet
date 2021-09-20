681a6c2edfef5027fe84ae681a4170481315ce69
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-09-19 23:03:13 -0300

    Criando servlet de listagem de empresas cadastradas

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -6,6 +6,15 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	
+	static {
+		Empresa empresa1 = new Empresa();
+		empresa1.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Caelum");
+		lista.add(empresa1);
+		lista.add(empresa2);
+	}
 
 	public void adicionaEmpresa(Empresa empresa) {
 		lista.add(empresa);
